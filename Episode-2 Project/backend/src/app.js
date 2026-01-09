cf2a356751a0a627febb8d97a82339985f0e4abd
rohitsolanki01 <rohitsolanki0473@gmail.com>
2026-01-09 23:09:18 +0530

    Something is added

--- Episode-2 Project/backend/src/app.js
+++ Episode-2 Project/backend/src/app.js
@@ -10,17 +10,55 @@ app.get("/test" , (req,res) => {
 });
 
 
-app.get("/user/:userId/:name/:nickname",(req,res) => {
-    console.log(req.params);                
-    res.send({
-        firstName : "Rohit",
-        lastName : "Solnaki",
-        city : "Wadhwan"
-    })
-});
+// app.get("/user/:userId/:name/:nickname",(req,res) => {
+//     console.log(req.params);                
+//     res.send({
+//         firstName : "Rohit",
+//         lastName : "Solnaki",
+//         city : "Wadhwan"
+//     })
+// });
+
+
+// app.use(
+//   "/user",
+//   (req, res,next) => {
+//     console.log("Handling the router!!");
+//     next();
+//     res.send("Response ");
+//   },
+//   (req, res) => {
+//     console.log("Handling the second Router");
+//     res.send("Response from the second router");
+//   }
+// );
 
 
 
+
+app.use("/user" , (req,res,next) => {
+    console.log("Handale route first 1 !!!");
+    next();
+},
+(req,res,next) => {
+console.log("Handale route second 2 !!!");
+next()
+},
+(req,res,next) => {
+    console.log("Handale route third 3 !!!");
+    next()
+},
+(req,res) => {
+    console.log("Handale route fourth 4 !!!");
+res.send("Response from the fourth router");    
+}
+
+);
+
+app.get("/user/:id" , (req,res) => {
+    console.log(req.params);
+    res.send("User Route");
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
