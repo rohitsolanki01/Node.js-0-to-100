0e5282ec9683fe78310c65816b753c9493dc6ca2
rohitsolanki01 <rohitsolanki0473@gmail.com>
2026-01-10 16:26:42 +0530

    writing auth middleware

--- Episode-2 Project/backend/src/app.js
+++ Episode-2 Project/backend/src/app.js
@@ -3,63 +3,25 @@ const app = express();
 
 const PORT = 8080;
 
+import {adminAuth , userAuth} from "./middlewares/authMiddleware.js" 
+
+app.use("/admin" , adminAuth);
 
 
-app.get("/test" , (req,res) => {
-    res.send("Test Route for  the Server")
-});
-
-
-// app.get("/user/:userId/:name/:nickname",(req,res) => {
-//     console.log(req.params);                
-//     res.send({
-//         firstName : "Rohit",
-//         lastName : "Solnaki",
-//         city : "Wadhwan"
-//     })
-// });
-
-
-// app.use(
-//   "/user",
-//   (req, res,next) => {
-//     console.log("Handling the router!!");
-//     next();
-//     res.send("Response ");
-//   },
-//   (req, res) => {
-//     console.log("Handling the second Router");
-//     res.send("Response from the second router");
-//   }
-// );
-
-
-
-
-app.use("/user" , (req,res,next) => {
-    console.log("Handale route first 1 !!!");
-    next();
-},
-(req,res,next) => {
-console.log("Handale route second 2 !!!");
-next()
-},
-(req,res,next) => {
-    console.log("Handale route third 3 !!!");
-    next()
-},
-(req,res) => {
-    console.log("Handale route fourth 4 !!!");
-res.send("Response from the fourth router");    
-}
-
-);
-
-app.get("/user/:id" , (req,res) => {
-    console.log(req.params);
-    res.send("User Route");
+app.get("/admin/getAllData" , (req,res) => {
+    res.send("Admin getAllData")
 })
 
-app.listen(PORT, () => {
+app.delete("/admin/deleteUser" , (req,res) => {
+    res.send("Admin  is delete User")
+})
+
+app.get("/user" , userAuth, (req,res) => {
+    res.send("User is get all Data")
+})
+
+
+
+app.listen(PORT , () => {
     console.log(`Server is running on port ${PORT}`);
 })
